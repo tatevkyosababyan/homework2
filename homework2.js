28e22fb1e6501a3cdcf7cadef97a1ee4d5ef9493
Student Generic Account <ADStudent@auaad.aua.am>
2017-09-09 18:24:03 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,9 @@
+console.log(5*11);
+const x=11
+const y=27
+console.log(x+y);
+const cloths=16
+const shoes=165
+if (cloths>shoes) {alert("go buy shoes!!!");}
+else if (cloths===shoes) {alert("you rock");}
+else {alert("go buy cloths!!!");}
